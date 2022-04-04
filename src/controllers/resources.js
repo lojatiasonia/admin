5abd5d058dacc48b0b6d2834ff2c95a87e91c9d5
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-04-04 11:23:39 -0300

    Update resources.js

--- src/controllers/resources.js
+++ src/controllers/resources.js
@@ -737,6 +737,11 @@ export default function () {
               ',birth_date,gender,phones,doc_number,orders_count,orders_total_value'
             break
           case 'orders':
+            // url += '&fields=_id,buyers.display_name,buyers.main_email,created_at'
+            // essa linha de cima, busca apenas o primeiro nome do cliente, o email e a data de criação do pedido
+            // url += '&created_at>=2022-03-01T03:00:00.000Z&created_at<=2022-04-01T02:59:59.000Z&fields=number,buyers._id,buyers.main_email,items.name,items.quantity,amount,status,financial_status.current,payment_method_label,extra_discount.discount_coupon,utm,created_at'
+            // essa de cima, estou filtrando por exemplo pra listar todos os pedidos que estão dentro de created_at do mês de março para buscar todos os pedidos desse período, mas somente informações específicas
+            // para propriedades específicas, você pode olhar https://developers.e-com.plus/docs/api/#/store/orders/
             url += '&fields=_id,created_at,checkout_link,utm,source_name,number,code,status' +
               ',financial_status,fulfillment_status,amount,payment_method_label,shipping_method_label'
             break
@@ -757,7 +762,6 @@ export default function () {
               result.forEach(doc => {
                 // add to list parsed to dot notation
                 delete doc.store_id
-                delete doc.created_at
                 delete doc.updated_at
                 exportData.push(parseDocToRow(doc))
               })
